02b80d7c2338e932a8a7ccddf3a91e1e064bdbe6
ekem57 <ekem57@hotmail.com>
2024-02-09 23:18:40 +0300

    dashboard viewcomponent add

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context context = new Context();
+            ViewBag.v1 = context.Blogs.Count().ToString();
+            ViewBag.v2 = context.Blogs.Where(x => x.WriterId ==1).Count().ToString();
+            ViewBag.v3 = context.Categories.Count().ToString();
+
             return View();
         }
     }
