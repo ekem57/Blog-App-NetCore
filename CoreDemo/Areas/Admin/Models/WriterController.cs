3d6c476380e83c88e8b9cff1d100cdd61c33be6d
ekem57 <ekem57@hotmail.com>
2024-02-16 22:24:46 +0300

    project add ajax

---
+++ CoreDemo/Areas/Admin/Models/WriterController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreDemo.Areas.Admin.Models
+{
+    [Area("Admin")]
+
+    public class WriterController :Controller
+    {
+       public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetWriterByID(int id)
+        {
+            var findWriters = writers.FirstOrDefault(x => x.Id == id);
+            var jsonWriters = JsonConvert.SerializeObject(findWriters);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult AddWriter(WriterClass w)
+        {
+            writers.Add(w);
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriters = JsonConvert.SerializeObject(w);
+            return Json(jsonWriters);
+        }
+
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+                Id =1,
+                Name ="Ayşe"
+            },
+             new WriterClass
+            {
+                Id =2,
+                Name ="Ahmet"
+            },
+              new WriterClass
+            {
+                Id =3,
+                Name ="Buse"
+            },
+
+        };
+    }
+}
